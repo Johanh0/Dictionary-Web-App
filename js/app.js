a8d4a98aab4e8b6c91f43a053a72b37d2b1eaced
Johanh0 <johanherrera@Johans-MBP.lan>
2024-02-25 16:58:42 -0500

    font change is ready.

--- js/app.js
+++ js/app.js
@@ -1,28 +1,81 @@
 const html = document.querySelector("html");
 const toggleBtn = document.querySelector(".toggle-btn");
+const selectDropdown = document.querySelector("#select-dropdown");
+
+// console.log(selectDropdown.selectedIndex);
+
+function localStorageFontFamily() {
+  if (localStorage.getItem("font-family")) {
+    const fontFamily = parseInt(localStorage.getItem("font-family"));
+    switch (fontFamily) {
+      case 0:
+        html.style.setProperty("--font-family", `"Inter", sans-serif`);
+        break;
+      case 1:
+        html.style.setProperty("--font-family", `"Lora", serif`);
+        break;
+      case 2:
+        html.style.setProperty("--font-family", `"Inconsolata", monospace`);
+        break;
+    }
+  }
+}
 
 function isUserDarkMode() {
-  if (window.matchMedia("(prefers-color-scheme: ligth)").matches) {
-    html.dataset.darkmode = "ligth";
+  const isDarkModeSave = localStorage.getItem("darkmode");
+  if (isDarkModeSave === "light") {
+    html.dataset.darkmode = "light";
     toggleBtn.style.transform = "translateX(0px)";
+    return;
+  } else if (isDarkModeSave === "dark") {
+    html.dataset.darkmode = "dark";
+    toggleBtn.style.transform = "translateX(24px)";
+    return;
+  }
+
+  if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+    html.dataset.darkmode = "light";
+    toggleBtn.style.transform = "translateX(0px)";
+    return;
   } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
     html.dataset.darkmode = "dark";
     toggleBtn.style.transform = "translateX(24px)";
+    return;
   }
 }
 
 function toggleDarkMode() {
-  if (html.dataset.darkmode === "ligth") {
+  if (html.dataset.darkmode === "light") {
     html.dataset.darkmode = "dark";
     toggleBtn.style.transform = "translateX(24px)";
+    localStorage.setItem("darkmode", "dark");
   } else if (html.dataset.darkmode === "dark") {
-    html.dataset.darkmode = "ligth";
+    html.dataset.darkmode = "light";
     toggleBtn.style.transform = "translateX(0px)";
+    localStorage.setItem("darkmode", "light");
   }
+}
+
+function changeFontFamily() {
+  switch (selectDropdown.selectedIndex) {
+    case 0:
+      html.style.setProperty("--font-family", `"Inter", sans-serif`);
+      break;
+    case 1:
+      html.style.setProperty("--font-family", `"Lora", serif`);
+      break;
+    case 2:
+      html.style.setProperty("--font-family", `"Inconsolata", monospace`);
+      break;
+  }
+
+  localStorage.setItem("font-family", selectDropdown.selectedIndex);
 }
 
 window.addEventListener("load", () => {
   isUserDarkMode();
+  localStorageFontFamily();
 
   toggleBtn.addEventListener("click", toggleDarkMode);
+  selectDropdown.addEventListener("change", changeFontFamily);
 });
